736cd231a3161f5b374c7cd3254e93837e360b9f
Olga Maslova <olga.maslova56@yandex.ru>
2023-06-23 22:06:38 +0300

    feat "пройти покрытие тестами v1.3"

---
+++ src/test/java/ru/practicum/shareit/request/RequestServiceIntegTest.java
@@ -0,0 +1,41 @@
+package ru.practicum.shareit.request;
+
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import ru.practicum.shareit.request.dto.*;
+import ru.practicum.shareit.request.service.ItemRequestService;
+import ru.practicum.shareit.user.dto.*;
+import ru.practicum.shareit.user.service.UserService;
+
+@SpringBootTest(
+        properties = "db.name=test",
+        webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+public class RequestServiceIntegTest {
+    private final ItemRequestService requestService;
+    private final UserService userService;
+
+    @BeforeEach
+    public void setUp() {
+        UserDtoRequest userDto = new UserDtoRequest(1L,
+                "mail@gmail.com",
+                "name"
+        );
+        userService.addUser(userDto);
+    }
+
+    @Test
+    public void shouldSuccessAddRequest() {
+        UserDtoResponse requestorDto = userService.getUser(1L);
+        ItemRequestDto itemRequest = new ItemRequestDto("Allneded thing");
+
+        ItemRequestResponseDto newItemRequest = requestService.addItemRequest(1, itemRequest);
+
+        Assertions.assertNotNull(newItemRequest);
+        Assertions.assertEquals(newItemRequest.getDescription(), itemRequest.getDescription());
+    }
+}
