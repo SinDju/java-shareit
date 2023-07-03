15cb6425e04c5ab355085d75352efdcd567b4d39
Maslova Olga <olga.maslova56@yandex.ru>
2023-07-03 14:21:40 +0300

    feax "добавить хендлер для обработки всех необработанных исключенийм1.0"

--- server/src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
+++ server/src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -7,21 +7,25 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.Map;
+
 @Slf4j
 @RestControllerAdvice
 public class ErrorHandler {
+
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public Map<String, String> handleHappinessOverflow(final ObjectBadRequestException e) {
         log.debug("Получен статус 400 Bad request {}", e.getMessage(), e);
         return Map.of("Ошибка запроса: {}", e.getMessage());
     }
+
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public Map<String, String> handleNotFoundException(final ObjectNotFoundException e) {
         log.debug("Получен статус 404 Not found {}", e.getMessage(), e);
         return Map.of("Ошибка запроса: {}", e.getMessage());
     }
+
     @ExceptionHandler
     @ResponseStatus(HttpStatus.FORBIDDEN)
     public Map<String, String> handleNotFoundException(final ObjectForbiddenException e) {
