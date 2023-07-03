8d8dab2ad5713dc7abbd505807c2b0987fba1427
Maslova Olga <olga.maslova56@yandex.ru>
2023-07-03 14:13:44 +0300

    feax "добавить хендлер для обработки всех необработанных исключений"

--- server/src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
+++ server/src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -7,29 +7,32 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.Map;
-
 @Slf4j
 @RestControllerAdvice
 public class ErrorHandler {
-
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public Map<String, String> handleHappinessOverflow(final ObjectBadRequestException e) {
         log.debug("Получен статус 400 Bad request {}", e.getMessage(), e);
         return Map.of("Ошибка запроса: {}", e.getMessage());
     }
-
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public Map<String, String> handleNotFoundException(final ObjectNotFoundException e) {
         log.debug("Получен статус 404 Not found {}", e.getMessage(), e);
         return Map.of("Ошибка запроса: {}", e.getMessage());
     }
-
     @ExceptionHandler
     @ResponseStatus(HttpStatus.FORBIDDEN)
     public Map<String, String> handleNotFoundException(final ObjectForbiddenException e) {
         log.debug("Получен статус 403 Forbidden {}", e.getMessage(), e);
+        return Map.of("Ошибка запроса: {}", e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Map<String, String> handleThrowableException(final Exception e) {
+        log.debug("Получен статус 500 Internal Server Error {}", e.getMessage(), e);
         return Map.of("Ошибка запроса: {}", e.getMessage());
     }
 }
